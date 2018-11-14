dc224d532c404000eae0bfc4c10563d5964bd4e0
Josue Ruiz <josue_zu@outlook.es>
2018-11-14 13:26:55 -0600

    Avance del codigo del MLP

--- Codigo/main.m
+++ Codigo/main.m
@@ -1,16 +1,55 @@
 addpath("./Funciones/")
 
-%Pedir archivos%
+% 1a y 1b Pedir archivos%
 archivo_entrada=input('Ingrese el nombre del archivo con los datos de entrada: ','s');
 ruta='Archivos/';
 archivo_entrada=strcat(ruta,archivo_entrada);
 
 datos_entrada=importdata(archivo_entrada);
+[filas_entrada,columnas_entrada]=size(datos_entrada);
 
 archivo_entrada=input('Ingrese el nombre del archivo con los targets: ','s');
 ruta='Archivos/';
 archivo_entrada=strcat(ruta,archivo_entrada);
 
 targets=importdata(archivo_entrada);
+[filas_target,columnas_target]=size(targets);
+
 %Fin de la peticion de entradas%
+
+% 1c Se pide el rango de la señal%
+limite_inferior=input('Limite inferior de la senal: ');
+limite_superior=input('Limite superior de la senal: ');
+if(limite_inferior<=limite_superior)
+	rango=limite_inferior:1/(filas_entrada):limite_superior;
+else
+	rango=limite_superior:1/(filas_entrada):limite_inferior;
+end 
+%Fin del modulo%
+
+% 1d Pedir vectores de arquitectura%
+vector_arquitectura=input('Vector de arquitectura: ');
+vector_funciones=input('Vector de funciones: ');
+%Fin del modulo 1d%
+
+% 1e Pedir valor del factor de aprendizaje%
+factor_aprendizaje=input('Ingrese el factor de aprendizaje: ');
+%Fin del modulo 1e%
+
+%1f valores de las condiciones de finalizacion%
+epocas=input('Numero de epocas: ');
+error_epoch_max=input('Senal de error: ');
+epoch_val=input('Cada cuantas iteraciones se llevara a cabo una epoca de validacion: ');
+num_val=input('Numero maximo de incrementos consecutivos del error de epoca de validacion: ');
+%Fin del modulo 1f%
+
+%1g Division del dataset en 3%
+opcion=input('Escoja la opcion que guste: \n1. 80%-10%-10%\n2. 70%-15%-15%\n');
+[conjunto_entrenamiento,conjunto_validacion,conjunto_prueba,target_entrenamiento,target_validacion,target_prueba]=separar_datos(datos_entrada,targets,opcion,filas_entrada);
+%Fin del modulo 1g%
+
+conjunto_prueba
+conjunto_validacion
+conjunto_entrenamiento
+
 
