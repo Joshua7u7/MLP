e3bd06fe571a92c5b293b7c1cb246894f6ad3c50
Dam2398 <44934386+Dam2398@users.noreply.github.com>
2018-11-19 17:17:06 -0600

    Add files via upload

--- main.m
+++ main.m
@@ -41,7 +41,7 @@ sumaerror=0;
         contador=0;
         e(:)=0;
         for dato=1:x_C_V
-            a{1}=Funcion(V2(1),W{1},p(C_V(dato)),b{1});
+            a{1}=Funcion(V2(1),W{1},C_V(dato),b{1});
             for i=2:y-1
                 a{i}=Funcion(V2(i),W{i},a{i-1},b{i});
             end
@@ -67,20 +67,20 @@ sumaerror=0;
         e(:)=0;
         for dato=1:x_C_E
         
-        a{1}=Funcion(V2(1),W{1},p(C_E(dato)),b{1});
-        for i=2:y-1
+        a{1}=Funcion(V2(1),W{1},C_E(dato),b{1});
+        for i=2:y2
             a{i}=Funcion(V2(i),W{i},a{i-1},b{i});
         end
         e(dato)=T_E(dato)-a{y2};
 
-        S{y2}=Funcion2(V2(y2),0,0,a{y2},t(C_E(dato)));
+        S{y2}=Funcion2(V2(y2),0,0,a{y2},T_E(dato));
         j=y2-1;
         while j>0
-            S{j}=Funcion2(V2(y2),W{j+1},S{j+1},a{j},t(C_E(dato)));
+            S{j}=Funcion2(V2(j),W{j+1},S{j+1},a{j},T_E(dato));
             j=j-1;
         end
 
-        W{1} = W{1}-alfa*S{1}*p(C_E(dato))';
+        W{1} = W{1}-alfa*S{1}*C_E(dato);
         b{1} = b{1}-alfa*S{1};
             for j=2:y2
                 W{j} = W{j}-alfa*S{j}*a{j-1}';
@@ -101,7 +101,7 @@ e(:)=0;
 sumaerror=0;
 for dato=1:x_C_P
     
-    a{1}=Funcion(V2(1),W{1},p(C_P(dato)),b{1});
+    a{1}=Funcion(V2(1),W{1},C_P(dato),b{1});
             for i=2:y-1
                 a{i}=Funcion(V2(i),W{i},a{i-1},b{i});
             end
